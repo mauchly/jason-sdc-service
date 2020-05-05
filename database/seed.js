dd169003db955933dc93b3b64cdd2456460c4e59
bettergrammer <wiesnerjjason@gmail.com>
2020-05-05 16:03:38 -0700

    Add 70M records to MySQL

--- database/seed.js
+++ database/seed.js
@@ -6,8 +6,6 @@ const EventEmitter = require('events').EventEmitter.defaultMaxListeners = 100;
 var price = faker.commerce.price(100,180.00,2)
 console.log('price', price)
 
-
-
 var listingTableData = `id, listingId, pricePerNight, weekend, weekendPrice, maxGuests, tax\n`;
 var bookingTableData = `id, listingId, nights, month, checkIn, checkOut, guests, children, infants\n`;
 var arrOfCalendarDays = [];
@@ -133,98 +131,97 @@ var toFillBookingsTable = function (arr, listingId) {
     listingId++;
   }
   // console.log('bookingTableData', bookingTableData)
-
-
-
 };
 
-//console.log('listingTableData', listingTableData)
-// var schema = `
-// DROP DATABASE IF EXISTS reservation_service;
+// console.log('listingTableData', listingTableData)
 
-// CREATE DATABASE reservation_service;
+var schema = `
+DROP DATABASE IF EXISTS reservation_service;
 
-// USE reservation_service;
+CREATE DATABASE reservation_service;
 
-// CREATE TABLE listingItems (
-//   id int NOT NULL AUTO_INCREMENT,
-//   listingId int NOT NULL,
-//   pricePerNight DECIMAL(5, 2) NOT NULL,
-//   weekend boolean NOT NULL default 0,
-//   weekendPrice DECIMAL(3, 2) NOT NULL,
-//   maxGuests int NOT NULL,
-//   tax DECIMAL(3, 2) NOT NULL,
-//   PRIMARY KEY(id)
+USE reservation_service;
 
-// );
+CREATE TABLE listingItems (
+  id int NOT NULL AUTO_INCREMENT,
+  listingId int NOT NULL,
+  pricePerNight DECIMAL(5, 2) NOT NULL,
+  weekend boolean NOT NULL default 0,
+  weekendPrice DECIMAL(3, 2) NOT NULL,
+  maxGuests int NOT NULL,
+  tax DECIMAL(3, 2) NOT NULL,
+  PRIMARY KEY(id)
 
-// CREATE TABLE bookings (
-//   id int NOT NULL AUTO_INCREMENT,
-//   listingId int,
-//   nights int,
-//   month VARCHAR(4),
-//   checkIn VARCHAR(10),
-//   checkOut VARCHAR(10),
-//   guests int,
-//   children int default 0,
-//   infants int default 0,
-//   PRIMARY KEY(id)
-// );
+);
 
-// LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV'
-// INTO TABLE listingItems
-// FIELDS TERMINATED BY ','
-// ENCLOSED BY '"'
-// LINES TERMINATED BY '\n'
-// IGNORE 1 ROWS;
+CREATE TABLE bookings (
+  id int NOT NULL AUTO_INCREMENT,
+  listingId int,
+  nights int,
+  month VARCHAR(4),
+  checkIn VARCHAR(10),
+  checkOut VARCHAR(10),
+  guests int,
+  children int default 0,
+  infants int default 0,
+  PRIMARY KEY(id)
+);
 
-// LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV'
-// INTO TABLE bookings
-// FIELDS TERMINATED BY ','
-// ENCLOSED BY '"'
-// LINES TERMINATED BY '\n'
-// IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV'
+INTO TABLE listingItems
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
-// `
-// var writeSchema = function (callback) {
-//   fs.writeFile ('../schema.sql', schema, function (err, results) {
-//     if (err) {
-//       console.log('err', err);
-//     } else {
-//       console.log('success');
-//     };
-//   });
-// };
-//write Schema into Schema.sql file
-// writeSchema();
+LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV'
+INTO TABLE bookings
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
-// let myWriteStream = fs.createWriteStream('./database/listingInfoCSV');
+`;
+var writeSchema = function (callback) {
+  fs.writeFile ('../schema.sql', schema, function (err, results) {
+    if (err) {
+      console.log('err', err);
+    } else {
+      console.log('success');
+    };
+  });
+};
+
+// write Schema into Schema.sql file
+writeSchema();
+
+let myWriteStream = fs.createWriteStream('./database/listingInfoCSV');
 let bookingsStream = fs.createWriteStream('./database/bookingsInfoCSV');
 
-// let writeListingInfoCSV = () => {
-//   for (let i = 0; i < 1000; i++) {
-//     if (i === 0) {
-//       toFillListingItemsTable(currentListingId);
-//       let write = myWriteStream.write(listingTableData);
-//       if (!write) {
-//         myWriteStream.once('drain', writeListingInfoCSV);
-//       } else {
-//         writeListingInfoCSV();
-//       }
-//     } else {
-//       currentListingId+= 10000;
-//       listingTableData = '';
-//       toFillListingItemsTable(currentListingId)
-//       let write = myWriteStream.write(listingTableData);
-//       if (!write) {
-//         myWriteStream.once('drain', writeListingInfoCSV);
-//       } else {
-//         writeListingInfoCSV();
-//       }
-//     }
-//   }
-//     myWriteStream.end();
-// };
+let writeListingInfoCSV = () => {
+  for (let i = 0; i < 1000; i++) {
+    if (i === 0) {
+      toFillListingItemsTable(currentListingId);
+      let write = myWriteStream.write(listingTableData);
+      if (!write) {
+        myWriteStream.once('drain', writeListingInfoCSV);
+      } else {
+        writeListingInfoCSV();
+      }
+    } else {
+      currentListingId+= 10000;
+      listingTableData = '';
+      toFillListingItemsTable(currentListingId)
+      let write = myWriteStream.write(listingTableData);
+      if (!write) {
+        myWriteStream.once('drain', writeListingInfoCSV);
+      } else {
+        writeListingInfoCSV();
+      }
+    }
+  }
+    myWriteStream.end();
+};
 
 let writeBookingInfoCSV = function(cb) {
   for (let i = 0; i < 1000; i++) {
@@ -252,5 +249,5 @@ let writeBookingInfoCSV = function(cb) {
   bookingsStream.end();
 };
 
-// writeListingInfoCSV();
+writeListingInfoCSV();
 writeBookingInfoCSV();
