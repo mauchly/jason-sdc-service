a7caf99d0eddd442ab513e613ae68d4f670ef8d5
bettergrammer <wiesnerjjason@gmail.com>
2020-05-07 23:27:58 -0700

    Complete seeding postgresQL

--- schema.sql
+++ schema.sql
@@ -1,45 +1,85 @@
-DROP DATABASE IF EXISTS reservation_service;
+-- Postgres schema
 
-CREATE DATABASE reservation_service;
+--  Execute this file for Postgres from the command line by typing:
+--    psql -f schema.sql -U jasonjacob reservation_service
+--  with the database already created to generate and seed the tables.
 
-USE reservation_service;
+-- CREATE TABLE listingItems (
+--   id int NOT NULL,
+--   listingId int NOT NULL,
+--   pricePerNight DECIMAL(5, 2) NOT NULL,
+--   weekend boolean NOT NULL default 'false',
+--   weekendPrice DECIMAL(3, 2) NOT NULL,
+--   maxGuests int NOT NULL,
+--   tax DECIMAL(3, 2) NOT NULL,
+--   PRIMARY KEY(id)
+-- );
 
-CREATE TABLE listingItems (
-  id int NOT NULL AUTO_INCREMENT,
-  listingId int NOT NULL,
-  pricePerNight DECIMAL(5, 2) NOT NULL,
-  weekend boolean NOT NULL default 0,
-  weekendPrice DECIMAL(3, 2) NOT NULL,
-  maxGuests int NOT NULL,
-  tax DECIMAL(3, 2) NOT NULL,
-  PRIMARY KEY(id)
+-- CREATE TABLE bookings (
+--   id int NOT NULL,
+--   listingId int,
+--   nights int,
+--   month VARCHAR(5),
+--   checkIn VARCHAR(10),
+--   checkOut VARCHAR(10),
+--   guests int,
+--   children int default 0,
+--   infants int default 0,
+--   PRIMARY KEY(id)
+-- );
 
-);
+COPY listingItems
+FROM '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV' DELIMITER ',' CSV HEADER;
 
-CREATE TABLE bookings (
-  id int NOT NULL AUTO_INCREMENT,
-  listingId int,
-  nights int,
-  month VARCHAR(4),
-  checkIn VARCHAR(10),
-  checkOut VARCHAR(10),
-  guests int,
-  children int default 0,
-  infants int default 0,
-  PRIMARY KEY(id)
-);
+-- COPY bookings
+-- FROM '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV' DELIMITER ',' CSV HEADER;
 
-LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV'
-INTO TABLE listingItems
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+-- mysql schema
 
-LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV'
-INTO TABLE bookings
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+-- DROP DATABASE IF EXISTS reservation_service;
 
+-- CREATE DATABASE reservation_service;
+
+-- USE reservation_service;
+
+-- CREATE TABLE listingItems (
+--   id int NOT NULL AUTO_INCREMENT,
+--   listingId int NOT NULL,
+--   pricePerNight DECIMAL(5, 2) NOT NULL,
+--   weekend tinyint NOT NULL default 0,
+--   weekendPrice DECIMAL(3, 2) NOT NULL,
+--   maxGuests int NOT NULL,
+--   tax DECIMAL(3, 2) NOT NULL,
+--   PRIMARY KEY(id)
+-- );
+
+-- CREATE TABLE bookings (
+--   id int NOT NULL AUTO_INCREMENT,
+--   listingId int,
+--   nights int,
+--   month VARCHAR(4),
+--   checkIn VARCHAR(10),
+--   checkOut VARCHAR(10),
+--   guests int,
+--   children int default 0,
+--   infants int default 0,
+--   PRIMARY KEY(id)
+-- );
+
+-- LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV'
+-- INTO TABLE listingItems
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
+
+-- LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV'
+-- INTO TABLE bookings
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
+
+--  Execute this file for mysql from the command line by typing:
+--    mysql -u root < schema.sql
+--  to create the database and generate and seed the tables.
