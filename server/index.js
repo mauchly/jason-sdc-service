c6c4d8fd4b058b2b1cf6f5f6ccba72ceaca5a359
bettergrammer <wiesnerjjason@gmail.com>
2020-05-20 19:55:51 -0700

    Cleanup code

--- server/index.js
+++ server/index.js
@@ -10,18 +10,13 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
 app.use(bodyParser.json());
-
-
 app.use(express.static(__dirname + '/../client/dist'));
-
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-
 
 app.get('/listingInfo', (req, res) => {
   //should give listingId 10001 back to the client when page first renders
@@ -91,8 +86,7 @@ app.post('/getBookedDates', (req, res) => {
       res.status(202).end(stringifyResults);
     }
   })
-
-})
+});
 
 
 app.get('/:id', (req, res) => {
@@ -105,20 +99,13 @@ app.get('/:id', (req, res) => {
       res.end(results);
     }
   })
-})
-
-
-
-
-
-
-
+});
 
 var port = 3001;
 
 app.listen(port, () => {
   console.log(`server listening at ${port}`)
-})
+});
 
 
 module.exports = app;
