8a6162e476ac9212f10a945007836c43ed66290a
bettergrammer <wiesnerjjason@gmail.com>
2020-05-25 12:28:41 -0700

    Extract ReactDOM.render function out of index.jsx into its own module

---
+++ client/src/renderApp.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Reservation from './index.jsx';
+import '../dist/style.css';
+
+ReactDOM.render(<Reservation/>, document.getElementById('reservation'));
