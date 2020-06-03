5636a6f14786706dc48c874b8c35eab7da574100
bettergrammer <wiesnerjjason@gmail.com>
2020-06-03 12:35:50 -0700

    Comment out unnecessary code

--- database/seed.js
+++ database/seed.js
@@ -134,94 +134,94 @@ var toFillBookingsTable = function (arr, listingId) {
 
 // console.log('listingTableData', listingTableData)
 
-var schema = `
-// Postgres schema
+// var schema = `
+// // Postgres schema
 
-/*  Execute this file for Postgres from the command line by typing:
- *    psql -f schema.sql -U jasonjacob reservation_service
- *  with the database already created to generate the tables. */
+// /*  Execute this file for Postgres from the command line by typing:
+//  *    psql -f schema.sql -U jasonjacob reservation_service
+//  *  with the database already created to generate the tables. */
 
-CREATE TABLE listingItems (
-  id int NOT NULL,
-  listingId int NOT NULL,
-  pricePerNight DECIMAL(5, 2) NOT NULL,
-  weekend boolean NOT NULL default '0',
-  weekendPrice DECIMAL(3, 2) NOT NULL,
-  maxGuests int NOT NULL,
-  tax DECIMAL(3, 2) NOT NULL,
-  PRIMARY KEY(id)
-);
+// CREATE TABLE listingItems (
+//   id int NOT NULL,
+//   listingId int NOT NULL,
+//   pricePerNight DECIMAL(5, 2) NOT NULL,
+//   weekend boolean NOT NULL default '0',
+//   weekendPrice DECIMAL(3, 2) NOT NULL,
+//   maxGuests int NOT NULL,
+//   tax DECIMAL(3, 2) NOT NULL,
+//   PRIMARY KEY(id)
+// );
 
-CREATE TABLE bookings (
-  id int NOT NULL,
-  listingId int,
-  nights int,
-  month VARCHAR(4),
-  checkIn VARCHAR(10),
-  checkOut VARCHAR(10),
-  guests int,
-  children int default 0,
-  infants int default 0,
-  PRIMARY KEY(id)
-);
+// CREATE TABLE bookings (
+//   id int NOT NULL,
+//   listingId int,
+//   nights int,
+//   month VARCHAR(4),
+//   checkIn VARCHAR(10),
+//   checkOut VARCHAR(10),
+//   guests int,
+//   children int default 0,
+//   infants int default 0,
+//   PRIMARY KEY(id)
+// );
 
-COPY listingItems
-FROM '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV' DELIMITER ',' CSV HEADER;
+// COPY listingItems
+// FROM '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV' DELIMITER ',' CSV HEADER;
 
-COPY listingItems
-FROM '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV' DELIMITER ',' CSV HEADER;
+// COPY listingItems
+// FROM '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV' DELIMITER ',' CSV HEADER;
 
-// mysql schema
+// // mysql schema
 
--- DROP DATABASE IF EXISTS reservation_service;
+// -- DROP DATABASE IF EXISTS reservation_service;
 
--- CREATE DATABASE reservation_service;
+// -- CREATE DATABASE reservation_service;
 
--- USE reservation_service;
+// -- USE reservation_service;
 
--- CREATE TABLE listingItems (
---   id int NOT NULL AUTO_INCREMENT,
---   listingId int NOT NULL,
---   pricePerNight DECIMAL(5, 2) NOT NULL,
---   weekend tinyint NOT NULL default 0,
---   weekendPrice DECIMAL(3, 2) NOT NULL,
---   maxGuests int NOT NULL,
---   tax DECIMAL(3, 2) NOT NULL,
---   PRIMARY KEY(id)
--- );
+// -- CREATE TABLE listingItems (
+// --   id int NOT NULL AUTO_INCREMENT,
+// --   listingId int NOT NULL,
+// --   pricePerNight DECIMAL(5, 2) NOT NULL,
+// --   weekend tinyint NOT NULL default 0,
+// --   weekendPrice DECIMAL(3, 2) NOT NULL,
+// --   maxGuests int NOT NULL,
+// --   tax DECIMAL(3, 2) NOT NULL,
+// --   PRIMARY KEY(id)
+// -- );
 
--- CREATE TABLE bookings (
---   id int NOT NULL AUTO_INCREMENT,
---   listingId int,
---   nights int,
---   month VARCHAR(4),
---   checkIn VARCHAR(10),
---   checkOut VARCHAR(10),
---   guests int,
---   children int default 0,
---   infants int default 0,
---   PRIMARY KEY(id)
--- );
+// -- CREATE TABLE bookings (
+// --   id int NOT NULL AUTO_INCREMENT,
+// --   listingId int,
+// --   nights int,
+// --   month VARCHAR(4),
+// --   checkIn VARCHAR(10),
+// --   checkOut VARCHAR(10),
+// --   guests int,
+// --   children int default 0,
+// --   infants int default 0,
+// --   PRIMARY KEY(id)
+// -- );
 
--- LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV'
--- INTO TABLE listingItems
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+// -- LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/listingInfoCSV'
+// -- INTO TABLE listingItems
+// -- FIELDS TERMINATED BY ','
+// -- ENCLOSED BY '"'
+// -- LINES TERMINATED BY '\n'
+// -- IGNORE 1 ROWS;
 
--- LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV'
--- INTO TABLE bookings
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+// -- LOAD DATA LOCAL INFILE '/Users/jasonjacob/Desktop/seniorProjects/sdc/jason-sdc-service/database/bookingsInfoCSV'
+// -- INTO TABLE bookings
+// -- FIELDS TERMINATED BY ','
+// -- ENCLOSED BY '"'
+// -- LINES TERMINATED BY '\n'
+// -- IGNORE 1 ROWS;
 
-/*  Execute this file for mysql from the command line by typing:
- *    mysql -u root < schema.sql
- *  to create the database and the tables. */
+// /*  Execute this file for mysql from the command line by typing:
+//  *    mysql -u root < schema.sql
+//  *  to create the database and the tables. */
 
-`;
+// `;
 
 var writeSchema = function (callback) {
   fs.writeFile ('../schema.sql', schema, function (err, results) {
@@ -236,7 +236,7 @@ var writeSchema = function (callback) {
 // write Schema into Schema.sql file
 // writeSchema();
 
-// let myWriteStream = fs.createWriteStream('./database/listingInfoCSV');
+let myWriteStream = fs.createWriteStream('./database/listingInfoCSV');
 let bookingsStream = fs.createWriteStream('./database/bookingsInfoCSV');
 
 let writeListingInfoCSV = () => {
@@ -290,5 +290,5 @@ let writeBookingInfoCSV = function(cb) {
   bookingsStream.end();
 };
 
-// writeListingInfoCSV();
+writeListingInfoCSV();
 writeBookingInfoCSV();
