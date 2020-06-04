023aaebe47525a5ec29d6be1b1582889b99093e1
bettergrammer <wiesnerjjason@gmail.com>
2020-06-04 12:58:08 -0700

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -20,6 +20,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
+    // enabled: false,
     level: 'info'
   },
   /**
