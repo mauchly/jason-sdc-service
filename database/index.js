b5afe1fd472b01e69ffce6129bd71c0cf1d4c12e
bettergrammer <wiesnerjjason@gmail.com>
2020-06-06 10:14:18 -0700

    Add authSource to mongoose.connect

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://root:mongoPassword@ec2-18-144-25-101.us-west-1.compute.amazonaws.com:27017/reservation_service', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://root:mongoPassword@ec2-18-144-25-101.us-west-1.compute.amazonaws.com:27017/reservation_service?authSource=reservation_service', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', () => {
