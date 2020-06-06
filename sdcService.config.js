3330d28eb5f02359c2077e1874ec881104b2e9bc
bettergrammer <wiesnerjjason@gmail.com>
2020-06-06 01:48:39 -0700

    Add sdcService.config.js

---
+++ sdcService.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name        : "sdc-api",
+    script      : "./server/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+     "NODE_ENV": "production"
+    }
+  },
+  {
+    name       : "sdc-client",
+    script     : "./client/dist/bundle.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+      "NODE_ENV": "production"
+    }
+  }]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -28,27 +28,5 @@ module.exports = {
         }
       }
     ]
-  },
-  apps : [{
-    name        : "sdc-api",
-    script      : "./server/index.js",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-     "NODE_ENV": "production"
-    }
-  },
-  {
-    name       : "sdc-client",
-    script     : "./client/dist/bundle.js",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-      "NODE_ENV": "production"
-    }
-  }]
+  }
 };
