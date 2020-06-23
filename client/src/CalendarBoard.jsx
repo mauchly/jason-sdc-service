1811eb7b3627adc6f3ee24602ca082aa4baa6370
bettergrammer <wiesnerjjason@gmail.com>
2020-06-23 01:27:32 -0700

    Update react imports

--- client/src/CalendarBoard.jsx
+++ client/src/CalendarBoard.jsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import WeekRow from './WeekRow.jsx';
 //import createMonth from './help.js'
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import $ from 'jquery';
 // import '../dist/style.css';
 import CalendarBoard from './CalendarBoard.jsx';
