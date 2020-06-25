c7da1501cc20358b068f19f1c7ca71bbdf5da247
bettergrammer <wiesnerjjason@gmail.com>
2020-06-24 23:31:27 -0700

    Update consoles

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -75,7 +75,7 @@ class Reservation extends React.Component {
     var urlOne = 'http://ec2-52-53-179-46.us-west-1.compute.amazonaws.com/listingInfo';
     var windowUrlString = window.location.href;
     let searchedId = windowUrlString.split('/').pop();
-    console.log('searchedId', searchedId);
+    // console.log('searchedId', searchedId);
     if (searchedId < 1 || searchedId > 10000000) {
       listingId = 1
     } else {
@@ -207,11 +207,11 @@ class Reservation extends React.Component {
      success: (results) => {
        stillLoading = false;
        clearInterval(set);
-       console.log('success results', typeof results);
+      //  console.log('success results', typeof results);
        if (!Array.isArray(results)) {
          console.log('Could not get reviews, inserting hardcoded reviews...');
          this.setState({
-           reviews: ["5.00", " (100 reviews)"]
+           reviews: ["5.00", "(100 reviews)"]
          });
        } else {
          console.log('results', results);
